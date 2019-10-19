11a0e5bbcf534c65af1eca18a08548f386cb9301
DerikCarroll <derikcarroll@gmail.com>
2019-10-19 12:00:20 -0500

    Add object literal with necessary calculator functions and queryselector needed variables.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,34 @@
-// Enter a description of this program here.
+// Simple power level calculator for Destiny 2
 
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+const powercalc = {
+  add(a, b, c, d, e, f, g, h) {
+    return (
+      Number(a) +
+      Number(b) +
+      Number(c) +
+      Number(d) +
+      Number(e) +
+      Number(f) +
+      Number(g) +
+      Number(h)
+    );
+  },
+  divide(a) {
+    return a / 8;
+  },
+  remainder(a) {
+    return a % 8;
+  }
+};
+
+const form = document.querySelector("form");
+const h2 = document.querySelector("h2");
+const h3 = document.querySelector("h3");
+const kinetic = document.querySelector("#kinetic");
+const energy = document.querySelector("#energy");
+const heavy = document.querySelector("#heavy");
+const helmet = document.querySelector("#helmet");
+const gauntlet = document.querySelector("#gauntlet");
+const chest = document.querySelector("#chest");
+const legs = document.querySelector("#legs");
+const classitem = document.querySelector("#class");
